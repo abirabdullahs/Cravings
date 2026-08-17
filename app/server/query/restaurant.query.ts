d046920ffd6cf418e107c8ecd9e9e699018044ae
MD Abir Hossen Abdullah <87601654+abirabdullahs@users.noreply.github.com>
2026-08-18 03:56:31 +0600

    Delete restaurant.query.ts

--- app/server/query/restaurant.query.ts
+++
@@ -1,9 +0,0 @@
-export const Find_Restaurants = `SELECT * FROM RESTAURANTS R 
-WHERE ($1 IS NULL OR name ILIKE '%' || $1 || '%' OR description ILIKE '%' || $1 || '%') 
-AND ($2 IS NULL OR EXISTS (
-  SELECT 1 FROM MENUITEMS M 
-  JOIN CATEGORY C ON M.category_id = C.id
-  WHERE R.id = M.restaurant_id
-  AND C.name = $2)) 
-ORDER BY status DESC, rating DESC 
-LIMIT $4`;
